dffd1b61e13fdef2d44709bbde2fde04eda5ccb7
Paritosh Batish <batishparitosh2@gmail.com>
2026-04-29 03:47:42 +0530

    updated menu list view

--- restaurant_project/menu/views.py
+++ restaurant_project/menu/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import MenuItem
+
+
 # Create your views here.
 # menu/views.py
 
@@ -14,16 +17,29 @@ def home(request):
 
 
 def menu_list(request):
-    """A view that returns a simple menu."""
-    menu_items = """
-    <h1>Today's Menu</h1>
-    <ul>
-        <li>Pasta Carbonara - $15</li>
-        <li>Grilled Salmon - $22</li>
-        <li>Caesar Salad - $12</li>
-    </ul>
-    """
-    return HttpResponse(menu_items)
+    # """A view that returns a simple menu."""
+    # menu_items = """
+    # <h1>Today's Menu</h1>
+    # <ul>
+    #     <li>Pasta Carbonara - $15</li>
+    #     <li>Grilled Salmon - $22</li>
+    #     <li>Caesar Salad - $12</li>
+    # </ul>
+    # """
+    # return HttpResponse(menu_items)
+    # items = MenuItem.objects.all()
+    items = MenuItem.objects.filter(is_available=True)
+
+    response = "<h3>My menu items are:</h3>"
+    response += "\n<ul>"
+    for item in items:
+        response += f"\n<li>{item.name}</li>"
+
+    response += "\n</ul>"
+
+    return HttpResponse(response)
+
+
 
 # URL: /menu/item/42/
 def item_detail(request, item_id):  # item_id is passed automatically!
